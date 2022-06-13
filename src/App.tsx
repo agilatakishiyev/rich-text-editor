e9e507e6ec7c7dc7cc56b192d255decd39f6fdec
Agil Atakishiyev <aqilatakishiyev1@gmail.com>
2022-06-13 14:08:42 +0400

    fixed some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { Editor } from "./components/Editor";
+import Logo from "./images/logo512.png";
 
 function App() {
-  return <Editor />;
+  return (
+    <div className="wrapper">
+      <a href="/">
+        <img src={Logo} alt="" width={50} height={50} />
+      </a>
+
+      <Editor />
+    </div>
+  );
 }
 
 export default App;
